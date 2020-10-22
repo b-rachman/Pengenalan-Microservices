7642b5d54368dcb37142eecb6cc15c08c4e9d254
b-rachman <bobbyrachman97@outlook.com>
2020-10-22 16:40:27 +0700

    init db connection

--- menu-service/go.mod
+++ menu-service/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/FadhlanHawali/Digitalent-Kominfo_Pendalaman-Rest-API v0.0.0-20201018024122-8e10e3ab7ffe
 	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.4
 )
--- menu-service/main.go
+++ menu-service/main.go
@@ -5,16 +5,50 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/b-rachman/Pengenalan-Microservices/menu-service/config"
+	"github.com/b-rachman/Pengenalan-Microservices/menu-service/database"
 	"github.com/b-rachman/Pengenalan-Microservices/menu-service/handler"
 	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	cfg := config.Config{
+		Database: config.Database{
+			Driver:   "mysql",
+			Host:     "localhost",
+			Port:     "3306",
+			User:     "root",
+			Password: "12345",
+			DbName:   "pengenalan-microservice",
+			Config:   "charset=utf8&parseTime=True&loc=Local",
+		},
+	}
+
+	db, err := initDB(cfg.Database)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
 	router := mux.NewRouter()
-
 	router.Handle("/add-menu", http.HandlerFunc(handler.AddMenu))
-
 	fmt.Println("Menu Service Listern to :8000")
 	log.Panic(http.ListenAndServe(":8000", router))
+}
 
+func initDB(dbConfig config.Database) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.DbName, dbConfig.Config)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(database.Menu{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
